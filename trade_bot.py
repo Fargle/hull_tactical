434dc57ca80341f00ca4235cbae106d008279e0c
fargle <ian7campbell@gmail.com>
2020-03-25 15:01:45 -0700

    added trade bot, not functional

---
+++ trade_bot.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+class Model(nn.Module):
+    def __init__(self):
+        super.__init__()
+        
+if __name__ == '__main__':
+    model = Model()
