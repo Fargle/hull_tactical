48cef5bb9823834354d19ccdbe5821048c95b687
fargle <ian7campbell@gmail.com>
2020-03-26 13:28:52 -0700

    not much to say about these changes

--- trade_bot.py
+++ trade_bot.py
@@ -1,10 +1,16 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import pandas as pd 
 
 class Model(nn.Module):
     def __init__(self):
-        super.__init__()
-        
+        super(self, Model).__init__()
+        self.conv1 = nn.Conv2d() 
+
 if __name__ == '__main__':
-    model = Model()
+    try:
+        df = pd.read_csv("ucsbdata.csv") 
+    except: 
+        print('"results.csv": no such file')
+    
